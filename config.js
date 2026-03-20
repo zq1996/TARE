3fa34b1db92efbb804c2b7b89e04ad00f4347447
zhqi <571092931@qq.com>
2026-03-20 20:41:10 +0800

    修复vercel配置和线上地址

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const Config = {
         apiBase: 'http://localhost:3000'
     },
     production: {
-        apiBase: 'https://tare-project-9lhfuixbh-zq1996s-projects.vercel.app'
+        apiBase: 'https://taretool.vercel.app'
     },
     getCurrent() {
         const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
